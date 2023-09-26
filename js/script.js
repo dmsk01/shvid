dc13841073c87ec6d3d33a154989faf743263b3e
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2023-09-26 22:30:24 +0300

    add vimeo for preview showreel

--- js/script.js
+++ js/script.js
@@ -56,3 +56,14 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
 });
+
+$("body")
+  .on("click", ".vimeo-launch", function () {
+    let videoID = $(this).data("vimeo-id"),
+      $video = '<div id="vimeo-pop-container"><div><span class="vimeo-close"></span><iframe src="https://player.vimeo.com/video/' + videoID + '/?title=1&amp;byline=1&amp;portrait=0&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div></div>';
+    $("body").append($video);
+  })
+
+  .on("click", ".vimeo-close", function () {
+    $("#vimeo-pop-container").remove();
+  });
